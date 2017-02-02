f788465f52454856a50adf096f44da947c809cab
iandioch <iandioch11@gmail.com>
2017-02-02 19:08:25 +0000

    Add working

--- swap_lowest.py
+++ swap_lowest.py
@@ -11,9 +11,11 @@ def swap(newboard, x1, y1, x2, y2):
     return newboard
 
 def calc_score_of_swap(board):
+    score = 0
     for y in range(h-1, -1, -1):
-        for x in range(0, w-2):
-            print(board[y][x], board[y][x+1], board[y][x+2])
+        x = 0
+        while x < w-2:
+            #print(board[y][x], board[y][x+1], board[y][x+2])
             if board[y][x] == board[y][x+1]:
                 n = 2
                 for i in range(x+2, w):
@@ -21,10 +23,13 @@ def calc_score_of_swap(board):
                         break
                     n += 1
                 if n > 2:
-                    print ('found n', n)
-    for y in range(h-1, 1, -1):
-        for x in range(0, w):
-            print('checking vert ', y, x)
+                    x += n
+                    score += n**2
+            x += 1
+    for x in range(0, w):
+        y = h-1
+        while y > 1:
+            #print('checking vert ', y, x)
             if board[y][x] == board[y-1][x]:
                 n = 2
                 for i in range(y-2, -1, -1):
@@ -32,7 +37,10 @@ def calc_score_of_swap(board):
                         break
                     n += 1
                 if n > 2:
-                    print ('found vert n', n)
+                    y -= n
+                    score += n**2
+            y -= 1
+    return score
 
 buff = []
 board = []
@@ -49,7 +57,9 @@ for i in board:
 calc_score_of_swap(board)
 
 for i in range(1):
-    print(i)
+    #print(i)
+    bestscore = 0
+    bestquad = [0,0,0,0]
     for x in range(w-1):
         for y in range(h-2, -1, -1):
             newboard = copy2d(board)
@@ -61,5 +71,9 @@ for i in range(1):
             if board[y][x] == newboard[y][x]:
                 # no change
                 continue
-            print ('\n'.join([''.join([str(c) for c in j]) for j in newboard]))
-            calc_score_of_swap(newboard)
+            #print ('\n'.join([''.join([str(c) for c in j]) for j in newboard]))
+            score = calc_score_of_swap(newboard)
+            if score > bestscore:
+                bestscore = score
+                bestquad = [y1, x1, y2, x2]
+    print (','.join([str(t) for t in bestquad]))
